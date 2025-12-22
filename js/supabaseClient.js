ff7fa12a0896eeba1c9004ce2d5fcb76dded455a
aminedamoun <71056422+aminedamoun@users.noreply.github.com>
2025-12-22 22:58:29 +0400

    Updated supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://nybgvfskctuljqlrsmsq.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_zbJZxJVPQmkJYK3lK9LrBg_BuJTg1A';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55Ymd2ZnNrdGN1bGpxbHJzbXNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY0MjYwMTgsImV4cCI6MjA4MjAwMjAxOH0.mtGthHGaMrczVSyIysSTbnQioLdkq_mIPce_27KMjLg';
 
 window.supabase = supabase.createClient(
   SUPABASE_URL,
