3d55d085566320efedd7bb1f1c3d6496fb108fdd
aminedamoun <71056422+aminedamoun@users.noreply.github.com>
2025-12-22 23:23:23 +0400

    Updated supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -3,7 +3,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
 // ✅ USE YOUR REAL PROJECT
-const SUPABASE_URL = "https://owvufovfnngqdhbscoci.supabase.co";
+const SUPABASE_URL = "https://nybgvfsktculjqlrsmsq.supabase.co";
 
 // ✅ REAL ANON KEY (FROM SUPABASE → Settings → API → anon public)
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55Ymd2ZnNrdGN1bGpxbHJzbXNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY0MjYwMTgsImV4cCI6MjA4MjAwMjAxOH0.mtGthHGaMrczVSyIysSTbnQioLdkq_mIPce_27KMjLg";
