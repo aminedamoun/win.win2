7e5b2b0b299576126b3c4e10d772377037d9a3f4
aminedamoun <71056422+aminedamoun@users.noreply.github.com>
2025-12-22 22:12:53 +0400

    Updated login.html

---
+++ js/supabaseClient.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = 'https://nybgvfsktculjqlrsmsq.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55Ymd2ZnNrdGN1bGpxbHJzbXNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY0MjYwMTgsImV4cCI6MjA4MjAwMjAxOH0.mtGthHGaMrczVSyIysSTbnQioLdkq_mIPce_27KMjLg';
+
+const { createClient } = supabase;
+window.supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
