b7f9327be500acbd92b9862ff8d848833da134c0
aminedamoun <71056422+aminedamoun@users.noreply.github.com>
2025-12-23 00:52:29 +0400

    Updated supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "https://nybgvfsktculjqlrsmsq.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55Ymd2ZnNrdGN1bGpxbHJzbXNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY0MjYwMTgsImV4cCI6MjA4MjAwMjAxOH0.mtGthHGaMrczVSyIysSTbnQioLdkq_mIPce_27KMjLg";
+const SUPABASE_URL = "https://owvufovfnngqdhbscoci.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93dnVmb3Zmbm5ncWRoYnNjb2NpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY0MTEwOTQsImV4cCI6MjA4MTk4NzA5NH0.sDDk9dKx_jHCEArBsyFzOrEMbSfHHnmZmcMD8znP2_I";
 
 const { createClient } = window.supabase;
 window.supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
