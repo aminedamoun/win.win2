f0354156e44351a502109693aa5be8b71074749d
aminedamoun <71056422+aminedamoun@users.noreply.github.com>
2026-01-06 00:26:59 +0400

    Updated content-editor.html

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,7 +1,8 @@
 const SUPABASE_URL = "https://owvufovfnngqdhbscoci.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93dnVmb3Zmbm5ncWRoYnNjb2NpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY0MTEwOTQsImV4cCI6MjA4MTk4NzA5NH0.sDDk9dKx_jHCEArBsyFzOrEMbSfHHnmZmcMD8znP2_I";
 
-const { createClient } = window.supabase;
-window.supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-window.supabaseUrl = SUPABASE_URL;
-window.supabaseAnonKey = SUPABASE_ANON_KEY;
+if (!window.supabaseClient) {
+  const { createClient } = window.supabase;
+  window.supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+}
+window.supabase = window.supabaseClient;
